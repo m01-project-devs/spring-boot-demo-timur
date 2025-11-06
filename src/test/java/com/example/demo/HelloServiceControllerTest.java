83866755d2b7dd57a70b02ff6e35abadb435a17f
TimurSatarov <timursatarov3@gmail.com>
2025-11-06 12:29:19 +0100

    add test file(HelloServiceController)

---
+++ src/test/java/com/example/demo/HelloServiceControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import com.example.demo.service.HelloService;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloServiceControllerTest
+{
+    private final HelloService helloService = new HelloService();
+    @Test
+    void testGetGreeting_withOutValidName()
+    {
+        String greeting = helloService.getGreeting("");
+        assertEquals("Hello World!", greeting);
+    }
+    @Test
+    void testGetGreeting_withValidName()
+    {
+        String greeting = helloService.getGreeting("Timur");
+        assertEquals("Hello Timur!", greeting);
+    }
+}
